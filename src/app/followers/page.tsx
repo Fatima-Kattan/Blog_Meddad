3c59a2d8b8f9d4779335aeefc100d57f68b64b72
RayaNaSSi <rassi7266@gmail.com>
2026-01-11 18:49:43 +0300

    fix error in following + followers pages

--- src/app/followers/page.tsx
+++ src/app/followers/page.tsx
@@ -1,5 +1,5 @@
 
-import Followers from '@/components/follow/followers/followers'
+import Followers from '@/components/follow/followers/Followers'
 import React from 'react'
 // import LoginForm from '../../components/auth/login/LoginForm'
 
